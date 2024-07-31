907065639bd36ad1ade3bba3e7bf15edfa72c63b
gudir <pengetahuanburung@gmail.com>
2024-07-31 10:29:34 +0700

    chore: configre nodemailer email helper

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import express from 'express';
 import morgan from 'morgan';
 import connectionToDB from './config/connectDB.js';
 import { morganMiddleware, systemLogs } from './utils/Logger.js';
+import mongoSanitize from "express-mongo-sanitize";
 
 await connectionToDB();
 
@@ -17,6 +18,7 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(mongoSanitize());
 app.use(morganMiddleware)
 
 app.get('/api/v1/test', (req, res) => {
@@ -27,7 +29,7 @@ const PORT = process.env.PORT || 1997;
 
 app.listen(PORT, () => {
   console.log(
-    `${chalk.green.bold('✓')} 👍 Server running in ${chalk.yellow.bold(process.env.NODE_ENV)} mode on port ${chalk.blue.bold(PORT)}`
+    `${chalk.green.bold('✅')} 👍 Server running in ${chalk.yellow.bold(process.env.NODE_ENV)} mode on port ${chalk.blue.bold(PORT)}`
   );
   systemLogs.info(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
