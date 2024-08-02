63c22c2e6f8e130a5b60887a311448522f270259
gudir <pengetahuanburung@gmail.com>
2024-08-02 09:46:17 +0700

    feat: add resend verification email tokens

--- backend/controllers/auth/verifyEmailController.js
+++ backend/controllers/auth/verifyEmailController.js
@@ -1,4 +1,4 @@
-import asyncHandler from "express-async-handler";
+import asyncHandler from 'express-async-handler';
 import User from '../../models/userModel.js';
 import verifyResetTokenModel from '../../models/verifyResetTokenModel.js';
 import sendEmail from '../../utils/sendEmail.js';
@@ -10,49 +10,49 @@ const domainURL = process.env.DOMAIN;
 // $-auth    Public
 
 const verifyUserEmail = asyncHandler(async (req, res) => {
-    const user = await User.findOne({_id: req.params.userId }).select("-passwordConfirm");
+  const user = await User.findOne({ _id: req.params.userId }).select(
+    '-passwordConfirm'
+  );
 
-    if (!user) {
-		res.status(400);
-		throw new Error("We were unable to find a user for this token");
-	}
+  if (!user) {
+    res.status(400);
+    throw new Error('We were unable to find a user for this token');
+  }
 
-    if (user.isEmailVerified) {
-		res.status(400).send(
-			"This user has already been verified. Please login"
-		);
-	}
+  if (user.isEmailVerified) {
+    res.status(400).send('This user has already been verified. Please login');
+  }
 
-    const userToken = await verifyResetTokenModel.findOne({
-		_userId: user._id,
-		token: req.params.emailToken,
-	});
+  const userToken = await verifyResetTokenModel.findOne({
+    _userId: user._id,
+    token: req.params.emailToken,
+  });
 
-    if (!userToken) {
-		res.status(400);
-		throw new Error("Token invalid! Your token may have expired");
-	}
+  if (!userToken) {
+    res.status(400);
+    throw new Error('Token invalid! Your token may have expired');
+  }
 
-    user.isEmailVerified = true;
-    await user.save();
+  user.isEmailVerified = true;
+  await user.save();
 
-    if (user.isEmailVerified) {
-        const emailLink = `${domainURL}/login`;
+  if (user.isEmailVerified) {
+    const emailLink = `${domainURL}/login`;
 
-        const payload = {
-            name: user.firstName,
-            link: emailLink
-        }
+    const payload = {
+      name: user.firstName,
+      link: emailLink,
+    };
 
-        await sendEmail(
-            user.email,
-            "Welcome - Account Verified",
-            payload,
-            "./emails/template/welcome.handlebars"
-        );
+    await sendEmail(
+      user.email,
+      'Welcome - Account Verified',
+      payload,
+      './emails/template/welcome.handlebars'
+    );
 
-        res.redirect("/auth/verify"); //<-- ini link front end
-    }
+    res.redirect('/auth/verify'); //<-- ini link front end
+  }
 });
 
 export default verifyUserEmail;
