e47e85a0b18bc0d28b5c419ac41344d416885226
gudir <pengetahuanburung@gmail.com>
2024-08-03 11:37:53 +0700

    feat: deactivate user admin only

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,6 +6,7 @@ import deleteMyAccount from '../controllers/user/deleteMyAccountController.js';
 import role from '../middleware/roleMiddleware.js';
 import getAllUserAccounts from '../controllers/user/getAllUserAccountsController.js';
 import deleteUserAccount from '../controllers/user/deleteUserAccountController.js';
+import deactivateUser from '../controllers/user/deactivateUserController.js';
 
 const router = express.Router();
 
@@ -23,5 +24,9 @@ router
 router
   .route('/:id')
   .delete(checkAuth, role.checkRole(role.ROLES.Admin), deleteUserAccount);
+
+router
+  .route('/:id/deactivate')
+  .put(checkAuth, role.checkRole(role.ROLES.Admin), deactivateUser);
 
 export default router;
